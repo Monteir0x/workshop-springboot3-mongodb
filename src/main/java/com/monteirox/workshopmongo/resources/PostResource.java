3ce8fc1b1f207baff070b38e6311437ddcd91664
Monteir0x <msmonteiro@id.uff.br>
2023-06-21 15:44:25 -0300

    Consulta com varios criterios

--- src/main/java/com/monteirox/workshopmongo/resources/PostResource.java
+++ src/main/java/com/monteirox/workshopmongo/resources/PostResource.java
@@ -9,6 +9,7 @@ import com.monteirox.workshopmongo.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,6 +33,19 @@ public class PostResource {
     public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
         text = URL.decodePAram(text);
         List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate)
+    {
+        text = URL.decodePAram(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = postService.fullSearch(text, min, max);
         return ResponseEntity.ok().body(list);
     }
 }
